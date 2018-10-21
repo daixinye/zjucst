8fa745d825d577cb8210a813f6c14c5ee6925c76
Dxy <denight@qq.com>
2018-10-21 20:56:58 +0800

    update: cal.m v3, printMonthCal with specific month & year

--- ios-development/playgournd/cal.m
+++ ios-development/playgournd/cal.m
@@ -1,4 +1,6 @@
 #import <Foundation/Foundation.h>
+#import <stdio.h>
+
 
 bool validateYear(char* cyear){
     int year = [[NSString stringWithUTF8String:cyear] intValue];
@@ -44,9 +46,63 @@ int getCurrentMonth(){
     return month;
 }
 
+int getMonthFisrtDaysWeekday(int month, int year){
+    NSDate *date = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comps;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    NSTimeZone *timeZone = [NSTimeZone localTimeZone];
+    [formatter setTimeZone:timeZone];
+    [formatter setDateFormat:@"M/d/yyyy"];
+    NSString *firstDay = [NSString stringWithFormat:@"%d/%d/%d",month,1,year];
+    date = [formatter dateFromString:firstDay];
+    [formatter release];
+    // 注意：星期日是1，星期一是2，以此类推
+    comps = [calendar components:(NSCalendarUnitWeekday) fromDate:date];
+    NSInteger weekday = [comps weekday];
+    return weekday;
+}
+
+int getMonthLastDay(int month, int year){
+    NSDate *date = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comps;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    NSTimeZone *timeZone = [NSTimeZone localTimeZone];
+    [formatter setTimeZone:timeZone];
+    [formatter setDateFormat:@"M/d/yyyy"];
+    NSString *firstDay = [NSString stringWithFormat:@"%d/%d/%d",month,1,year];
+    date = [formatter dateFromString:firstDay];
+    // 调用rangeOfUnit方法:(返回一样是一个结构体)两个参数一个大单位，一个小单位(.length就是天数，.location就是月)
+    NSInteger monthNum = [calendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:date].length;
+    return monthNum;
+}
+
+void print(NSString *s){
+    const char *c = [s UTF8String];
+    printf("%s",c);
+}
+
 void printCal(int month, int year){
-    
-    NSLog(@"printCal: %d %d", month, year);
+    NSArray *MONTH_MAP = @[@"一月",@"二月",@"三月",@"四月",@"五月",@"六月",@"七月", @"八月", @"九月",@"十月", @"十一月",@"十二月"];
+
+    print([NSString stringWithFormat:@"      %@ %d\n", MONTH_MAP[month-1], year]);
+    printf("日 一 二 三 四 五 六\n");
+    int firstDayWeekday = getMonthFisrtDaysWeekday(month, year);
+    for(int i = 1; i < firstDayWeekday; i++){
+        // 前导空格
+        printf("   ");
+    }
+    int lastDay = getMonthLastDay(month, year);
+    for(int j = 1; j <= lastDay; j++){
+        printf("%2d", j);
+        if((j+firstDayWeekday-1)%7 == 0){
+            printf("\n");
+        }else {
+            printf(" ");
+        }
+    }
+    printf("\n\n");
 }
 
 void printYearCal(int year){
