533f215f73faf98a16c4dd46cac20ed37cc0135a
Dxy <denight@qq.com>
2018-10-21 19:57:04 +0800

    update: cal.m, update error handling

--- ios-development/playgournd/cal.m
+++ ios-development/playgournd/cal.m
@@ -1,19 +1,47 @@
 #import <Foundation/Foundation.h>
 
-bool validateYear(int year){
-    return year >= 1 && year <= 9999;
+bool validateYear(char* cyear){
+    int year = [[NSString stringWithUTF8String:cyear] intValue];
+    if(year >= 1 && year <= 9999){
+        return true;
+    }else{
+        NSLog(@"cal: year `%s` not in range 1..9999", cyear);
+        return false;
+    }
 }
 
-bool validateMonth(int month){
-    return month >= 1 && month <= 12;
+bool validateMonth(char* cmonth){
+    int month = [[NSString stringWithUTF8String: cmonth] intValue];
+    if(month >= 1 && month <= 12){
+        return true;
+    }else{
+        NSLog(@"cal: %s is not a month number (1..12)", cmonth);
+        return false;
+    }
 }
 
 int getCurrentYear(){
-    return 2018;
+    //获得当前日期
+    NSDate *date = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comps;
+    
+    //获取本年
+    comps = [calendar components:(NSCalendarUnitYear) fromDate:date];
+    NSInteger year = [comps year];
+    return year;
 }
 
 int getCurrentMonth(){
-    return 10;
+    //获得当前日期
+    NSDate *date = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comps;
+
+    //获取本月
+    comps = [calendar components:(NSCalendarUnitMonth) fromDate:date];
+    NSInteger month = [comps month];
+    return month;
 }
 
 void printCal(int month, int year){
@@ -39,40 +67,36 @@ int main(int argc, char* argv[]){
                 break;
             case 2:
                 // usecase:4 输入：<year> 输出：当年年历
-                year = [[NSString stringWithUTF8String:argv[1]] intValue];
                 // validate: <year>
-                if(!validateYear(year)){
-                    NSLog(@"cal: year `%s` not in range 1..9999", argv[1]);
+                if(validateYear(argv[1])){
+                    year = [[NSString stringWithUTF8String:argv[1]] intValue];
+                    printYearCal(year);
                     return 1;
                 }
-                printYearCal(year);
                 break;
             case 3:
                 if(argv[1][0] == '-'){
                     // usecase:3 输入: -m <month> 输出：当年该月
                     if([@"-m" compare: [NSString stringWithUTF8String:argv[1]]] == NSOrderedSame){
-                        year = getCurrentYear();
-                        month = [[NSString stringWithUTF8String:argv[2]] intValue];
-                        printCal(month, year);
+                        // validate: <month>
+                        if(validateMonth(argv[2])){
+                            year = getCurrentYear();
+                            month = [[NSString stringWithUTF8String:argv[2]] intValue];
+                            printCal(month, year);
+                        }
                     }else{
+                        // validate: <option>
                         NSLog(@"cal: illegal option");
                         return 1;
                     }
                 }else{
                     // usecase:2 输入：<month> <year> 输出：该年该月月历
-                    month = [[NSString stringWithUTF8String:argv[1]] intValue];
-                    year = [[NSString stringWithUTF8String:argv[2]] intValue];
-                    // validate: <month>
-                    if(!validateMonth(month)){
-                        NSLog(@"cal: %s is not a month number (1..12)", argv[1]);
-                        return 1;
+                    // validate: <month> & <year>
+                    if(validateMonth(argv[1]) && validateYear(argv[2])){
+                        month = [[NSString stringWithUTF8String:argv[1]] intValue];
+                        year = [[NSString stringWithUTF8String:argv[2]] intValue];
+                        printCal(month, year);
                     }
-                    // validate: <year>
-                    if(!validateYear(year)){
-                        NSLog(@"cal: year `%s` not in range 1..9999", argv[2]);
-                        return 1;
-                    }
-                    printCal(month, year);
                 }
                 break;
             
