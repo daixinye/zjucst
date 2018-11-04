26d5448be69c944730dcf89c0dbcf9f541591d6e
Dxy <denight@qq.com>
2018-11-04 16:01:07 +0800

    update: rename top30-mapreduce to top30keyword-mapreduce, add top10url-mapreduce

---
+++ big-data/hadoop/top10url-mapreduce.1/mapper.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    string key, value;
+    string line;
+    map<string, int> count_map;
+    while (getline(cin, line))
+    {
+        string ts, uid, keyword, rank, ord, url, year, month, day, hour;
+        istringstream iss(line);
+        iss >> ts >> uid >> keyword >> rank >> ord >> url >> year >> month >> day >> hour;
+
+        if (count_map.find(url) != count_map.end())
+        {
+            count_map[url]++;
+        }
+        else
+        {
+            count_map[url] = 1;
+        }
+    }
+
+    map<string, int>::iterator it = count_map.begin();
+    while (it != count_map.end())
+    {
+        cout<<it->first<<"\t"<<it->second<<endl;
+        it++;
+    }
+    return 0;
+}
