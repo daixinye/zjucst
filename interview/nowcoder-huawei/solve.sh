b0197c2c9b6fdd0e45153aae3892486564839929
daixinye <denight@qq.com>
2019-03-02 10:49:12 +0800

    init nowcoder-huawei

---
+++ interview/nowcoder-huawei/solve.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+name=$1
+echo $name
+
+echo "#include <iostream>" >> ./source/$name.cpp
+echo "using namespace std;" >> ./source/$name.cpp 
+echo "" >> ./source/$name.cpp 
+echo "int main() {" >> ./source/$name.cpp 
+echo "  return 0;" >> ./source/$name.cpp
+echo "}" >> ./source/$name.cpp
